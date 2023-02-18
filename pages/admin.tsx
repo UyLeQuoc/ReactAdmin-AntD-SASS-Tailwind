828b8e350c80bc93b827baff13fe9c4d17a29533
satoshiuy <lequocuyit@gmail.com>
2023-02-18 09:58:15 +0700

    update order page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,7 +2,7 @@
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 
-const App = dynamic(() => import("../components/Admin/AdminPage"), { ssr: false });
+const App = dynamic(() => import("/components/Admin/AdminPage"), { ssr: false })
 
 const Home: NextPage = () => {
   return <App />;
